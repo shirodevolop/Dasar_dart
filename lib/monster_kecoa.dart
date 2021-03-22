d190d3ea7643de362bf6c7f52ecbbcdf3aff246e
shirodevolop <emilioelva11@gmail.com>
2021-03-22 10:26:28 +0900

    dasar pbo

---
+++ lib/monster_kecoa.dart
@@ -0,0 +1,13 @@
+import 'package:dart_pbo/monster.dart';
+import 'package:dart_pbo/flying_monster.dart';
+
+class MonsterKecoa extends Monster implements FlyingMonster {
+
+  @override
+  String fly() => "Syuuuuuuung ......";
+
+  @override 
+  String move() {
+    return "jalan jalan santay";
+  }
+}
